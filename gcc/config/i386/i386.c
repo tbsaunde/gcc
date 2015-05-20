97b67526a05f242efed7b3ba67c013f9151df654
H.J. Lu <hjl.tools@gmail.com>
2015-05-20 10:28:39 -0700

    Don't increase alignments of objects on stack
    We should never increase alignments of objects on stack for Intel MCU.
    There is nothing to gain.
    
    	* config/i386/i386.c (ix86_local_alignment): Don't increase
    	alignment for Intel MCU.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -27432,6 +27432,10 @@ ix86_local_alignment (tree exp, machine_mode mode,
       return align;
     }
 
+  /* Don't increase alignment for Intel MCU psABI.  */
+  if (TARGET_IAMCU)
+    return align;
+
   /* x86-64 ABI requires arrays greater than 16 bytes to be aligned
      to 16byte boundary.  Exact wording is:
 
